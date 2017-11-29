3476f9deaddeb8b29d69b9a2d1956b336569d01b
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-11-29 14:36:24 -0500

    Add comments to test-deploy.sh

--- test-deploy.sh
+++ test-deploy.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 
+# Exit immediately if any of the following commands exits with a non-zero
+# status. This means that the Codeship build will fail as soon as one of the
+# commands below fails.
 set -e
 
-# Authenticate with the Google Services
+# Authenticate with Google Cloud using the credentials stored in your
+# application's repository.
 codeship_google authenticate
+
+# `codeship_google authenticate` writes the decrypted Google Cloud credentials
+# to /keyconfig.json. We specify the GOOGLE_APPLICATION_CREDENTIALS environment
+# variable so that the Cloud SQL Proxy can connect to your application's
+# production database.
 export GOOGLE_APPLICATION_CREDENTIALS=/keyconfig.json
 
-# switch to the directory containing your app.yml (or similar) configuration file
-# note that your repository is mounted as a volume to the /deploy directory
+# Switch directories to /deploy, which contains your application (including
+# app.yaml, the Google App Engine configuration file).
 cd /deploy/
 
-# test static asset precompilation
-RAILS_ENV=production bundle exec rake assets:precompile
+# Running `gcloud app deploy` causes your application's static assets to be
+# compiled. We test this command here.
+bundle exec rake assets:precompile RAILS_ENV=production
 
-# start the Google Cloud SQL proxy
+# Start the Cloud SQL Proxy, writing logs to a log file.
 cloud-sql-proxy -dir /cloudsql > log/cloudsql.log 2>&1 &
+
+# Read the Cloud SQL Proxy log file until a line containing "Ready for new
+# connections" is found. Then, continue to the next line.
 (tail -f log/cloudsql.log &) | sed '/Ready for new connections/q'
 
-# test the connection to the database
-RAILS_ENV=production rake db:version
+# Try to connect to the production database.
+rake db:version RAILS_ENV=production
 
-# test migrating the database
-DB_ADMIN=true RAILS_ENV=production rake db:migrate --dry-run
+# Perform a dry run of the pending migrations against the production database.
+rake db:migrate --dry-run RAILS_ENV=production
