db7da317f1210feb48e690b7c2a38d4f7f9285b5
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-12-01 14:05:01 -0500

    Modify deploy script to match

--- deploy.sh
+++ deploy.sh
@@ -27,12 +27,34 @@ gcloud app deploy --quiet
 # Start the Cloud SQL Proxy, writing logs to a log file.
 cloud-sql-proxy -dir /cloudsql > log/cloudsql.log 2>&1 &
 
-# Read the Cloud SQL Proxy log file until a line containing "Ready for new
-# connections" is found. Then, continue to the next line.
-(tail -f log/cloudsql.log &) | sed '/Ready for new connections/q'
+# Set cloud_sql_proxy_pid to the PID of the last command, i.e. running
+# cloud-sql-proxy.
+cloud_sql_proxy_pid=$!
 
-# Migrate the production database. Set the DB_ADMIN environment variable to
-# indicate that the command should connect to the database using a user that
-# has sufficient privileges to migrate the database:
-# http://jldbasa.github.io/blog/2014/02/14/mysql-user-minimum-required-privileges-for-rails/
-DB_ADMIN=true RAILS_ENV=production rake db:migrate
+# Time out after 10 seconds.
+for (( i = 0; i < 10; i++ )); do
+
+  # If the Cloud SQL Proxy log file contains 'Ready for new connections', the
+  # Cloud SQL Proxy is ready to access the production database.
+  if cat log/cloudsql.log | grep -q 'Ready for new connections'; then
+
+    echo "Ready to connect to the production database."
+
+    # Migrate the production database. Set the DB_ADMIN environment variable to
+    # indicate that the command should connect to the database using a user that
+    # has sufficient privileges to migrate the database:
+    # http://jldbasa.github.io/blog/2014/02/14/mysql-user-minimum-required-privileges-for-rails/
+    DB_ADMIN=true RAILS_ENV=production rake db:migrate
+
+    exit 0
+
+  fi
+
+  sleep 1
+
+done
+
+echo "Cloud SQL Proxy didn't connect to the production database within 10 seconds. Timing out..."
+
+# If the Cloud SQL Proxy exited, indicate that there is an error.
+exit 1
